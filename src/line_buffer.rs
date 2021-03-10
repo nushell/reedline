329bb972f2656ef401b9301440b665bb310ac339
Jason Rodney Hansen <jasonrodneyhansen@gmail.com>
2021-03-09 18:58:12 -0700

    Improve inc/dec of insertion point
    Removed need to collect grapheme indices into a Vec. Also, we can slice
    the buffer from or to the insertion point to not have to iterate through
    all of the indices.

--- src/line_buffer.rs
+++ src/line_buffer.rs
@@ -35,40 +35,27 @@ impl LineBuffer {
         self.insertion_point
     }
 
-    fn get_grapheme_indices(&self) -> Vec<(usize, &str)> {
-        UnicodeSegmentation::grapheme_indices(self.buffer.as_str(), true).collect()
-    }
-
     pub fn inc_insertion_point(&mut self) {
-        let grapheme_indices = self.get_grapheme_indices();
-        for i in 0..grapheme_indices.len() {
-            if grapheme_indices[i].0 == self.insertion_point && i < (grapheme_indices.len() - 1) {
-                self.insertion_point = grapheme_indices[i + 1].0;
-                return;
-            }
-        }
-        self.insertion_point = self.buffer.len();
-
-        //TODO if we should have found the boundary but didn't, we should panic
+        self.insertion_point = if let Some((i, _)) = self.buffer[self.insertion_point..]
+            .grapheme_indices(true)
+            .skip(1)
+            .next()
+        {
+            self.insertion_point + i
+        } else {
+            self.buffer.len()
+        };
     }
 
     pub fn dec_insertion_point(&mut self) {
-        let grapheme_indices = self.get_grapheme_indices();
-        if self.insertion_point == self.buffer.len() {
-            if let Some(index_pair) = grapheme_indices.last() {
-                self.insertion_point = index_pair.0;
-            } else {
-                self.insertion_point = 0;
-            }
+        self.insertion_point = if let Some((i, _)) = self.buffer[..self.insertion_point]
+            .grapheme_indices(true)
+            .last()
+        {
+            i
         } else {
-            for i in 0..grapheme_indices.len() {
-                if grapheme_indices[i].0 == self.insertion_point && i > 1 {
-                    self.insertion_point = grapheme_indices[i - 1].0;
-                    return;
-                }
-            }
-            self.insertion_point = 0;
-        }
+            0
+        };
     }
 
     pub fn get_buffer_len(&self) -> usize {
