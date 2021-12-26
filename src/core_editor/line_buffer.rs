47d1109181b0fb05ced1390ba5f05378d29022ad
sholderbach <sholderbach@users.noreply.github.com>
2021-12-26 17:49:29 +0100

    Fix formatting

--- src/core_editor/line_buffer.rs
+++ src/core_editor/line_buffer.rs
@@ -462,7 +462,9 @@ impl LineBuffer {
         }
 
         let search_str = &self.lines[self.grapheme_right_index()..];
-        search_str.find(*c).map(|index| index + self.grapheme_right_index())
+        search_str
+            .find(*c)
+            .map(|index| index + self.grapheme_right_index())
     }
 
     /// Finds index for the first occurrence of a char to the left of offset
