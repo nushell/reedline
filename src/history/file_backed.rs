57402a769652028a51335c4367079478f8bcd750
sholderbach <sholderbach@users.noreply.github.com>
2022-02-05 23:14:11 +0100

    Add safe `sync` method to `History`
    This method both reads from the file and writes local additions. Is safe
    when multiple `History` instances try to truncate the history file on
    disk.
    
    To avoid race conditions on the file uses the `fd_lock` crate for file
    advisory locks. (Inspiration for that thanks to rustyline: https://github.com/kkawakam/rustyline/blob/master/src/history.rs)

--- src/history/file_backed.rs
+++ src/history/file_backed.rs
@@ -2,8 +2,9 @@ use super::{base::HistoryNavigationQuery, History};
 use crate::core_editor::LineBuffer;
 use std::{
     collections::{vec_deque::Iter, VecDeque},
-    fs::{File, OpenOptions},
-    io::{BufRead, BufReader, BufWriter, Write},
+    fs::OpenOptions,
+    io::{BufRead, BufReader, BufWriter, Seek, SeekFrom, Write},
+    ops::{Deref, DerefMut},
     path::PathBuf,
 };
 
@@ -23,8 +24,7 @@ pub struct FileBackedHistory {
     entries: VecDeque<String>,
     cursor: usize, // If cursor == entries.len() outside history browsing
     file: Option<PathBuf>,
-    len_on_disk: usize,  // Keep track what was previously written to disk
-    truncate_file: bool, // as long as the file would not exceed capacity we can use appending writes
+    len_on_disk: usize, // Keep track what was previously written to disk
     query: HistoryNavigationQuery,
 }
 
@@ -62,7 +62,6 @@ impl History for FileBackedHistory {
                 // before adding a new one.
                 self.entries.pop_front();
                 self.len_on_disk = self.len_on_disk.saturating_sub(1);
-                self.truncate_file = true;
             }
             self.entries.push_back(entry.to_string());
         }
@@ -147,7 +146,6 @@ impl FileBackedHistory {
             cursor: 0,
             file: None,
             len_on_disk: 0,
-            truncate_file: true,
             query: HistoryNavigationQuery::Normal(LineBuffer::default()),
         }
     }
@@ -166,51 +164,8 @@ impl FileBackedHistory {
             std::fs::create_dir_all(base_dir)?;
         }
         hist.file = Some(file);
-        hist.load_file()?;
+        hist.sync()?;
         Ok(hist)
-    }
-
-    /// Loads history from the associated newline separated file
-    ///
-    /// Expects the [`History`] to be empty.
-    ///
-    ///
-    /// **Side effect:** creates not yet existing file.
-    fn load_file(&mut self) -> std::io::Result<()> {
-        let f = File::open(
-            self.file
-                .as_ref()
-                .expect("History::load_file should only be called if a filename is set"),
-        );
-        assert!(
-            self.entries.is_empty(),
-            "History currently designed to load file once in the constructor"
-        );
-        match f {
-            Err(e) => match e.kind() {
-                std::io::ErrorKind::NotFound => {
-                    File::create(self.file.as_ref().unwrap())?;
-                    Ok(())
-                }
-                _ => Err(e),
-            },
-            Ok(file) => {
-                let reader = BufReader::new(file);
-                let mut from_file = reader
-                    .lines()
-                    .map(|o| o.map(|i| decode_entry(&i)))
-                    .collect::<Result<VecDeque<String>, _>>()?;
-                let from_file = if from_file.len() > self.capacity {
-                    from_file.split_off(from_file.len() - self.capacity)
-                } else {
-                    from_file
-                };
-                self.len_on_disk = from_file.len();
-                self.entries = from_file;
-                self.reset_cursor();
-                Ok(())
-            }
-        }
     }
 
     fn back_with_criteria(&mut self, criteria: &dyn Fn(&str) -> bool) {
@@ -249,32 +204,65 @@ impl FileBackedHistory {
     /// Writes unwritten history contents to disk.
     ///
     /// If file would exceed `capacity` truncates the oldest entries.
-    fn flush(&mut self) -> std::io::Result<()> {
-        if self.file.is_none() {
-            return Ok(());
+    pub fn sync(&mut self) -> std::io::Result<()> {
+        if let Some(fname) = &self.file {
+            // The unwritten entries
+            let own_entries = self.entries.range(self.len_on_disk..);
+
+            let mut f_lock = fd_lock::RwLock::new(
+                OpenOptions::new()
+                    .create(true)
+                    .write(true)
+                    .read(true)
+                    .open(fname)?,
+            );
+            let mut writer_guard = f_lock.write()?;
+            let (mut foreign_entries, truncate) = {
+                let reader = BufReader::new(writer_guard.deref());
+                let mut from_file = reader
+                    .lines()
+                    .map(|o| o.map(|i| decode_entry(&i)))
+                    .collect::<Result<VecDeque<_>, _>>()?;
+                if from_file.len() + own_entries.len() > self.capacity {
+                    (
+                        from_file.split_off(from_file.len() - (self.capacity - own_entries.len())),
+                        true,
+                    )
+                } else {
+                    (from_file, false)
+                }
+            };
+
+            {
+                let mut writer = BufWriter::new(writer_guard.deref_mut());
+                if truncate {
+                    writer.seek(SeekFrom::Start(0))?;
+
+                    for line in &foreign_entries {
+                        writer.write_all(encode_entry(line).as_bytes())?;
+                        writer.write_all("\n".as_bytes())?;
+                    }
+                } else {
+                    writer.seek(SeekFrom::End(0))?;
+                }
+                for line in own_entries {
+                    writer.write_all(encode_entry(line).as_bytes())?;
+                    writer.write_all("\n".as_bytes())?;
+                }
+                writer.flush()?;
+            }
+            if truncate {
+                let file = writer_guard.deref_mut();
+                let file_len = file.stream_position()?;
+                file.set_len(file_len)?;
+            }
+
+            let own_entries = self.entries.drain(self.len_on_disk..);
+            foreign_entries.extend(own_entries);
+            self.entries = foreign_entries;
+
+            self.len_on_disk = self.entries.len();
         }
-        let file = if self.truncate_file {
-            // Rewrite the whole file if we truncated the old output
-            self.len_on_disk = 0;
-            // TODO: make this file race safe if multiple instances are used.
-            OpenOptions::new()
-                .write(true)
-                .truncate(true)
-                .open(self.file.as_ref().unwrap())?
-        } else {
-            // If the file is not beyond capacity just append new stuff
-            // (use the stored self.len_on_disk as offset)
-            OpenOptions::new()
-                .append(true)
-                .open(self.file.as_ref().unwrap())?
-        };
-        let mut writer = BufWriter::new(file);
-        for line in self.entries.range(self.len_on_disk..) {
-            writer.write_all(encode_entry(line).as_bytes())?;
-            writer.write_all("\n".as_bytes())?;
-        }
-        writer.flush()?;
-        self.len_on_disk = self.entries.len();
 
         Ok(())
     }
@@ -288,7 +276,7 @@ impl FileBackedHistory {
 impl Drop for FileBackedHistory {
     /// On drop the content of the [`History`] will be written to the file if specified via [`FileBackedHistory::with_file()`].
     fn drop(&mut self) {
-        let _res = self.flush();
+        let _res = self.sync();
     }
 }
 
