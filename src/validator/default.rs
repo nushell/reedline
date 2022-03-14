8ff014792767962eb1074776b4f979ac2c80e9a8
sholderbach <sholderbach@users.noreply.github.com>
2022-03-14 16:12:20 +0100

    Folderize validator

--- src/validator/default.rs
+++ src/validator/default.rs
@@ -1,20 +1,6 @@
-/// The syntax validation trait. Implementers of this trait will check to see if the current input
-/// is incomplete and spans multiple lines
-pub trait Validator: Send {
-    /// The action that will handle the current buffer as a line and return the corresponding validation
-    fn validate(&self, line: &str) -> ValidationResult;
-}
+use crate::{ValidationResult, Validator};
 
-/// Whether or not the validation shows the input was complete
-pub enum ValidationResult {
-    /// An incomplete input which may need to span multiple lines to be complete
-    Incomplete,
-
-    /// An input that is complete as-is
-    Complete,
-}
-
-/// A default validator which checks for mismatched quotes
+/// A default validator which checks for mismatched quotes and brackets
 pub struct DefaultValidator;
 
 impl Validator for DefaultValidator {
---
+++ src/validator/mod.rs
@@ -0,0 +1,18 @@
+mod default;
+pub use default::DefaultValidator;
+
+/// The syntax validation trait. Implementers of this trait will check to see if the current input
+/// is incomplete and spans multiple lines
+pub trait Validator: Send {
+    /// The action that will handle the current buffer as a line and return the corresponding validation
+    fn validate(&self, line: &str) -> ValidationResult;
+}
+
+/// Whether or not the validation shows the input was complete
+pub enum ValidationResult {
+    /// An incomplete input which may need to span multiple lines to be complete
+    Incomplete,
+
+    /// An input that is complete as-is
+    Complete,
+}
