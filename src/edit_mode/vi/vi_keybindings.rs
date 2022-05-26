fe795caabc5401d811006b93d5a6d4f220a049ff
Stefan Holderbach <sholderbach@users.noreply.github.com>
2022-05-26 23:17:50 +0200

    Organize the common keybindings (#433)
    Group the common keybindings into three functions:
    - `add_common_control_bindings`
    - `add_common_navigation_bindings`
    - `add_common_edit_bindings`
    
    Vi normal uses the first two
    Vi insert uses all of them
    
    Emacs extends upon those

--- src/edit_mode/vi/vi_keybindings.rs
+++ src/edit_mode/vi/vi_keybindings.rs
@@ -1,36 +1,28 @@
-use crate::{
-    edit_mode::{keybindings::add_common_keybindings, Keybindings},
-    ReedlineEvent,
-};
+use crossterm::event::{KeyCode, KeyModifiers};
 
-use crossterm::event::{KeyCode as KC, KeyModifiers as KM};
+use crate::{
+    edit_mode::{
+        keybindings::{
+            add_common_control_bindings, add_common_edit_bindings, add_common_navigation_bindings,
+            edit_bind,
+        },
+        Keybindings,
+    },
+    EditCommand,
+};
 
 /// Default Vi normal keybindings
 pub fn default_vi_normal_keybindings() -> Keybindings {
     let mut kb = Keybindings::new();
+    use EditCommand as EC;
+    use KeyCode as KC;
+    use KeyModifiers as KM;
 
-    kb.add_binding(KM::CONTROL, KC::Char('c'), ReedlineEvent::CtrlC);
-    kb.add_binding(KM::CONTROL, KC::Char('l'), ReedlineEvent::ClearScreen);
-    kb.add_binding(
-        KM::NONE,
-        KC::Up,
-        ReedlineEvent::UntilFound(vec![ReedlineEvent::MenuUp, ReedlineEvent::Up]),
-    );
-    kb.add_binding(
-        KM::NONE,
-        KC::Down,
-        ReedlineEvent::UntilFound(vec![ReedlineEvent::MenuDown, ReedlineEvent::Down]),
-    );
-    kb.add_binding(
-        KM::NONE,
-        KC::Left,
-        ReedlineEvent::UntilFound(vec![ReedlineEvent::MenuLeft, ReedlineEvent::Left]),
-    );
-    kb.add_binding(
-        KM::NONE,
-        KC::Right,
-        ReedlineEvent::UntilFound(vec![ReedlineEvent::MenuRight, ReedlineEvent::Right]),
-    );
+    add_common_control_bindings(&mut kb);
+    add_common_navigation_bindings(&mut kb);
+    // Replicate vi's default behavior for Backspace and delete
+    kb.add_binding(KM::NONE, KC::Backspace, edit_bind(EC::MoveLeft));
+    kb.add_binding(KM::NONE, KC::Delete, edit_bind(EC::Delete));
 
     kb
 }
@@ -39,7 +31,9 @@ pub fn default_vi_normal_keybindings() -> Keybindings {
 pub fn default_vi_insert_keybindings() -> Keybindings {
     let mut kb = Keybindings::new();
 
-    add_common_keybindings(&mut kb);
+    add_common_control_bindings(&mut kb);
+    add_common_navigation_bindings(&mut kb);
+    add_common_edit_bindings(&mut kb);
 
     kb
 }
