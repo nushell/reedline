211c5cf15ab08a06a8e86ff8f09ac7619ad05c92
Per Lindgren <per.lindgren@ltu.se>
2022-10-15 23:31:00 +0200

    examples updated

---
+++ examples/hinter.rs
@@ -0,0 +1,31 @@
+// Create a reedline object with in-line hint support.
+// cargo run --example hinter
+//
+// Fish-style history based hinting.
+// assuming history ["abc", "ade"]
+// pressing "a" hints to abc.
+// Up/Down or Ctrl p/n, to select next/previous match
+
+use nu_ansi_term::{Color, Style};
+use reedline::{DefaultHinter, DefaultPrompt, Reedline, Signal};
+use std::io;
+
+fn main() -> io::Result<()> {
+    let mut line_editor = Reedline::create().with_hinter(Box::new(
+        DefaultHinter::default().with_style(Style::new().italic().fg(Color::LightGray)),
+    ));
+    let prompt = DefaultPrompt::default();
+
+    loop {
+        let sig = line_editor.read_line(&prompt)?;
+        match sig {
+            Signal::Success(buffer) => {
+                println!("We processed: {}", buffer);
+            }
+            Signal::CtrlD | Signal::CtrlC => {
+                println!("\nAborted!");
+                break Ok(());
+            }
+        }
+    }
+}
---
+++ examples/history.rs
@@ -0,0 +1,33 @@
+// Create a reedline object with history support, including history size limits.
+// cargo run --example history
+//
+// A file `history.txt` will be created (or replaced).
+// Allows for persistent loading of previous session.
+//
+// Browse history by Up/Down arrows or Ctrl-n/p
+
+use reedline::{DefaultPrompt, FileBackedHistory, Reedline, Signal};
+use std::io;
+
+fn main() -> io::Result<()> {
+    let history = Box::new(
+        FileBackedHistory::with_file(5, "history.txt".into())
+            .expect("Error configuring history with file"),
+    );
+
+    let mut line_editor = Reedline::create().with_history(history);
+    let prompt = DefaultPrompt::default();
+
+    loop {
+        let sig = line_editor.read_line(&prompt)?;
+        match sig {
+            Signal::Success(buffer) => {
+                println!("We processed: {}", buffer);
+            }
+            Signal::CtrlD | Signal::CtrlC => {
+                println!("\nAborted!");
+                break Ok(());
+            }
+        }
+    }
+}
