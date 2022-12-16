519578ea31889a8cfa6d94facae0c572a5ac9dd0
Carl Schierig <contact@carlschierig.com>
2022-12-16 18:22:48 +0100

    fix clippy error for CursorConfig

--- src/edit_mode/cursors.rs
+++ src/edit_mode/cursors.rs
@@ -2,6 +2,7 @@ use crossterm::cursor::CursorShape;
 
 /// Maps cursor shapes to each edit mode (emacs, vi normal & vi insert).
 /// If any of the fields is `None`, the cursor won't get changed by Reedline for that mode.
+#[derive(Default)]
 pub struct CursorConfig {
     /// The cursor to be used when in vi insert mode
     pub vi_insert: Option<CursorShape>,
@@ -9,14 +10,4 @@ pub struct CursorConfig {
     pub vi_normal: Option<CursorShape>,
     /// The cursor to be used when in emacs mode
     pub emacs: Option<CursorShape>,
-}
-
-impl Default for CursorConfig {
-    fn default() -> Self {
-        Self {
-            vi_insert: None,
-            vi_normal: None,
-            emacs: None,
-        }
-    }
 }
