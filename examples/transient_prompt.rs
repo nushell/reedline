2fe9f3559d7fbf0126155087dbf03c84c98237a0
ysthakur <45539777+ysthakur@users.noreply.github.com>
2023-08-23 10:20:03 -0400

    Make separate transient prompt example, restore custom prompt example

--- examples/transient_prompt.rs
+++ examples/transient_prompt.rs
@@ -1,7 +1,7 @@
-// Create a reedline object with a custom prompt.
-// cargo run --example custom_prompt
+// Create a reedline object with a transient prompt.
+// cargo run --example transient_prompt
 //
-// Pressing keys will increase the right prompt value
+// Prompts for previous lines will be replaced with a shorter prompt
 
 use reedline::{
     Prompt, PromptEditMode, PromptHistorySearch, PromptHistorySearchStatus, Reedline, Signal,
@@ -10,28 +10,37 @@ use std::{borrow::Cow, cell::Cell, io};
 
 // For custom prompt, implement the Prompt trait
 //
-// This example displays the number of keystrokes
-// or rather increments each time the prompt is rendered.
+// This example replaces the prompt for old lines with "!" as an
+// example of a transient prompt.
 #[derive(Clone)]
-pub struct CustomPrompt(Cell<u32>, &'static str);
+pub struct TransientPrompt {
+    /// Whether to show the transient prompt indicator instead of the normal one
+    show_transient: Cell<bool>,
+}
 pub static DEFAULT_MULTILINE_INDICATOR: &str = "::: ";
-impl Prompt for CustomPrompt {
+pub static NORMAL_PROMPT: &str = "(transient_prompt example)";
+pub static TRANSIENT_PROMPT: &str = "!";
+impl Prompt for TransientPrompt {
     fn render_prompt_left(&self) -> Cow<str> {
         {
-            Cow::Owned(self.1.to_string())
+            if self.show_transient.get() {
+                Cow::Owned(String::new())
+            } else {
+                Cow::Borrowed(NORMAL_PROMPT)
+            }
         }
     }
 
     fn render_prompt_right(&self) -> Cow<str> {
-        {
-            let old = self.0.get();
-            self.0.set(old + 1);
-            Cow::Owned(format!("[{old}]"))
-        }
+        Cow::Owned(String::new())
     }
 
     fn render_prompt_indicator(&self, _edit_mode: PromptEditMode) -> Cow<str> {
-        Cow::Owned(">".to_string())
+        if self.show_transient.get() {
+            Cow::Borrowed(TRANSIENT_PROMPT)
+        } else {
+            Cow::Owned(">".to_string())
+        }
     }
 
     fn render_prompt_multiline_indicator(&self) -> Cow<str> {
@@ -52,15 +61,26 @@ impl Prompt for CustomPrompt {
             prefix, history_search.term
         ))
     }
+
+    fn repaint_on_enter(&self) -> bool {
+        // This method is called whenever the user hits enter to go to the next
+        // line, so we want it to repaint and display the transient prompt
+        self.show_transient.set(true);
+        true
+    }
 }
 
 fn main() -> io::Result<()> {
-    println!("Custom prompt demo:\nAbort with Ctrl-C or Ctrl-D");
+    println!("Transient prompt demo:\nAbort with Ctrl-C or Ctrl-D");
     let mut line_editor = Reedline::create();
 
-    let prompt = CustomPrompt(Cell::new(0), "Custom Prompt");
+    let prompt = TransientPrompt {
+        show_transient: Cell::new(false),
+    };
 
     loop {
+        // We're on a new line, so make sure we're showing the normal prompt
+        prompt.show_transient.set(false);
         let sig = line_editor.read_line(&prompt)?;
         match sig {
             Signal::Success(buffer) => {
