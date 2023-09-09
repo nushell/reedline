b28183c4e784e0ff2f40f753f4aaa1be7f70f432
David Laban <alsuren@gmail.com>
2023-09-09 17:56:14 +0100

    control-w behaviour is not common between vi_insert and emacs

--- src/edit_mode/vi/vi_keybindings.rs
+++ src/edit_mode/vi/vi_keybindings.rs
@@ -30,10 +30,15 @@ pub fn default_vi_normal_keybindings() -> Keybindings {
 /// Default Vi insert keybindings
 pub fn default_vi_insert_keybindings() -> Keybindings {
     let mut kb = Keybindings::new();
+    use EditCommand as EC;
+    use KeyCode as KC;
+    use KeyModifiers as KM;
 
     add_common_control_bindings(&mut kb);
     add_common_navigation_bindings(&mut kb);
     add_common_edit_bindings(&mut kb);
+
+    kb.add_binding(KM::CONTROL, KC::Char('w'), edit_bind(EC::BackspaceWord));
 
     kb
 }
