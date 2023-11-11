50b58805887a45d9725cbdcea785351d8be44325
Clément Nerma <clementnerma@fastmail.com>
2023-11-11 11:07:41 +0100

    Make `Result` and `ReedlineError` public
    Allows implementing custom `History` types

--- src/lib.rs
+++ src/lib.rs
@@ -239,7 +239,7 @@ mod engine;
 pub use engine::Reedline;
 
 mod result;
-pub(crate) use result::Result;
+pub use result::{Result, ReedlineError};
 
 mod history;
 #[cfg(any(feature = "sqlite", feature = "sqlite-dynlib"))]
