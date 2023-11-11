ea18cd66d7a57a6440ddbf1454004b3ffad6d6d3
Clément Nerma <clementnerma@fastmail.com>
2023-11-11 22:52:27 +0100

    Add more derive impl for HistoryItemId

--- src/history/item.rs
+++ src/history/item.rs
@@ -5,7 +5,7 @@ use serde::{de::DeserializeOwned, Deserialize, Serialize};
 use std::{fmt::Display, time::Duration};
 
 /// Unique ID for the [`HistoryItem`]. More recent items have higher ids than older ones.
-#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct HistoryItemId(pub(crate) i64);
 impl HistoryItemId {
     pub(crate) const fn new(i: i64) -> HistoryItemId {
