75bdae3f60efd04cc21197bf8d13823a05f6b8b2
maxomatic458 <104733404+maxomatic458@users.noreply.github.com>
2024-01-20 19:12:51 +0100

    Revert "respect windows newline in update_values"
    This reverts commit 070d600545737b53bc9f48e7b9e93cf9fa8fc171.

--- src/menu/ide_menu.rs
+++ src/menu/ide_menu.rs
@@ -679,7 +679,7 @@ impl Menu for IdeMenu {
             // editing a multiline buffer.
             // Also, by replacing the new line character with a space, the insert
             // position is maintain in the line buffer.
-            let trimmed_buffer = editor.get_buffer().replace("\r\n", "  ").replace('\n', " ");
+            let trimmed_buffer = editor.get_buffer().replace('\n', " ");
             completer.complete(
                 &trimmed_buffer[..editor.insertion_point()],
                 editor.insertion_point(),
