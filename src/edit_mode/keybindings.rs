3c46dc2c0c69476a625611a556e67ddb8439629c
Stefan Holderbach <sholderbach@users.noreply.github.com>
2024-11-21 10:28:52 +0100

    Set `Ctrl-J`/`\n` to `ReedlineCommand::Enter` (#855)
    Permits terminal automation sending a `\n` to behave the same as if a
    `\r` is sent which is the canonical Enter according to ANSI/crossterm

--- src/edit_mode/keybindings.rs
+++ src/edit_mode/keybindings.rs
@@ -234,7 +234,7 @@ pub fn add_common_edit_bindings(kb: &mut Keybindings) {
     );
     kb.add_binding(KM::ALT, KC::Enter, edit_bind(EC::InsertNewline));
     kb.add_binding(KM::SHIFT, KC::Enter, edit_bind(EC::InsertNewline));
-    kb.add_binding(KM::CONTROL, KC::Char('j'), edit_bind(EC::InsertNewline));
+    kb.add_binding(KM::CONTROL, KC::Char('j'), ReedlineEvent::Enter);
 }
 
 pub fn add_common_selection_bindings(kb: &mut Keybindings) {
