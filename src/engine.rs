6a9229a9f51d67b4acb8dd299b8c0766804ebe4e
Emanuil Chizhov <echizhov@disroot.org>
2024-12-02 22:01:05 +0200

    Potential fix for live build

--- src/engine.rs
+++ src/engine.rs
@@ -1182,7 +1182,7 @@ impl Reedline {
                             Some(&EditCommand::Backspace)
                             | Some(&EditCommand::BackspaceWord)
                             | Some(&EditCommand::MoveToLineStart { select: false }) => {
-                                menu.menu_event(MenuEvent::Deactivate(self.modal_mode))
+                                menu.menu_event(MenuEvent::Deactivate(false))
                             }
                             _ => {
                                 menu.menu_event(MenuEvent::Edit(self.quick_completions));
