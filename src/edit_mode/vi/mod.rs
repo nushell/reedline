31603ee5759187fddc75c0372313b65d19709147
TechnoHouse (deephbz) <13776377+deephbz@users.noreply.github.com>
2024-12-31 11:45:43 +0800

    fix: align cut-related commands behavior with Vim standards

--- src/edit_mode/vi/mod.rs
+++ src/edit_mode/vi/mod.rs
@@ -89,11 +89,10 @@ impl EditMode for Vi {
                             self.cache.clear();
                             ReedlineEvent::None
                         } else if res.is_complete(self.mode) {
+                            let event = res.to_reedline_event(self);
                             if let Some(mode) = res.changes_mode() {
                                 self.mode = mode;
                             }
-
-                            let event = res.to_reedline_event(self);
                             self.cache.clear();
                             event
                         } else {
