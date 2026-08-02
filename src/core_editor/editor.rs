efea16c72c7316b0f4d73857f93d72b4d9ea3b43
Martin Kronberger <kronberger@proton.me>
2026-08-02 11:39:17 +0200

    test(editor): keep case-transform selections inside the buffer (#1140)
    `"FOO BAR BAZ"` is 11 bytes, thus selecting to 12 moves one past the
    end and trips the `debug_assert!(buf.is_char_boundary(pos))` that
    #1100 added to `prev_grapheme_boundary`.
    
    The cases were written before that assert existed, and since #1087
    last ran CI against that older base, they only began failing once
    the branch landed on main.

--- src/core_editor/editor.rs
+++ src/core_editor/editor.rs
@@ -3505,13 +3505,13 @@ mod test {
     #[rstest]
     #[case("foo bar baz", 0, 3, EditCommand::LowercaseSelection, "foo bar baz")]
     #[case("Foo Bar Baz", 0, 7, EditCommand::LowercaseSelection, "foo bar Baz")]
-    #[case("FOO BAR BAZ", 0, 12, EditCommand::LowercaseSelection, "foo bar baz")]
+    #[case("FOO BAR BAZ", 0, 11, EditCommand::LowercaseSelection, "foo bar baz")]
     #[case("foo bar baz", 0, 3, EditCommand::UppercaseSelection, "FOO bar baz")]
     #[case("Foo Bar Baz", 0, 7, EditCommand::UppercaseSelection, "FOO BAR Baz")]
-    #[case("FOO BAR BAZ", 0, 12, EditCommand::UppercaseSelection, "FOO BAR BAZ")]
+    #[case("FOO BAR BAZ", 0, 11, EditCommand::UppercaseSelection, "FOO BAR BAZ")]
     #[case("foo bar baz", 0, 3, EditCommand::SwitchcaseSelection, "FOO bar baz")]
     #[case("Foo Bar Baz", 0, 7, EditCommand::SwitchcaseSelection, "fOO bAR Baz")]
-    #[case("FOO BAR BAZ", 0, 12, EditCommand::SwitchcaseSelection, "foo bar baz")]
+    #[case("FOO BAR BAZ", 0, 11, EditCommand::SwitchcaseSelection, "foo bar baz")]
     fn test_lower_upper_switchcase_selection(
         #[case] input: &str,
         #[case] selection_start: usize,
