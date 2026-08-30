337c4e1cf5dd77a8ec4515ac9374d9953b58acf9
Gly <gabin.lefranc@gmail.com>
2026-08-30 21:54:32 +0200

    Clean text object code up
    Remove CutInsidePair, CutAroundPair, CopyInsidePair, CopyAroundPair and
    replace them by CutTextObject, CopyTextObject.
    Also clean the vi mode implementation to remove action inside/around
    pair

--- src/core_editor/editor.rs
+++ src/core_editor/editor.rs
@@ -406,10 +406,6 @@ impl Editor {
             }
             #[cfg(feature = "system_clipboard")]
             EditCommand::PasteSystem => self.paste_from_system(),
-            EditCommand::CutInsidePair { left, right } => self.cut_inside_pair(*left, *right),
-            EditCommand::CopyInsidePair { left, right } => self.copy_inside_pair(*left, *right),
-            EditCommand::CutAroundPair { left, right } => self.cut_around_pair(*left, *right),
-            EditCommand::CopyAroundPair { left, right } => self.copy_around_pair(*left, *right),
             EditCommand::CutTextObject { text_object } => self.cut_text_object(*text_object),
             EditCommand::CopyTextObject { text_object } => self.copy_text_object(*text_object),
             EditCommand::AddTextObject { text_object } => self.add_text_object(*text_object),
