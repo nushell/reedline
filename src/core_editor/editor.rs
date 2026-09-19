e0f76b1e9f4a00fee22e62b60b0416d3b6414623
Gly <gabin.lefranc@gmail.com>
2026-09-19 18:56:29 +0200

    Add/Remove/ReplaceTextObject edits the buffer instof MoveCursor in EditType

--- src/core_editor/editor.rs
+++ src/core_editor/editor.rs
@@ -379,7 +379,13 @@ impl Editor {
                     | EditCommand::LowercaseSelection
                     | EditCommand::UppercaseSelection
                     | EditCommand::SwitchcaseSelection
-            ) && self.edit_mode.retains_selection_after_edit());
+            ) && self.edit_mode.retains_selection_after_edit())
+            || matches!(
+                command,
+                EditCommand::ReplaceTextObject { .. }
+                    | EditCommand::CutTextObject { .. }
+                    | EditCommand::CopyTextObject { .. }
+            );
         if !leaves_selection {
             self.clear_selection();
         }
@@ -387,12 +393,6 @@ impl Editor {
         self.commit_cursor();
 
         let new_undo_behavior = match (command, command.edit_type()) {
-            (
-                EditCommand::AddTextObject { .. }
-                | EditCommand::RemoveTextObject { .. }
-                | EditCommand::ReplaceTextObject { .. },
-                EditType::MoveCursor { .. },
-            ) => UndoBehavior::CreateUndoPoint,
             (_, EditType::MoveCursor { .. }) => UndoBehavior::MoveCursor,
             (EditCommand::InsertChar(c), EditType::EditText) => UndoBehavior::InsertCharacter(*c),
             (EditCommand::Delete, EditType::EditText) => {
